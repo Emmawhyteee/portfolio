6436dbeec49a911913b1d112f943ab04fe5f4e6b
Nwankwo Emmanuel <emmatexiii@gmail.com>
2024-07-31 12:38:25 +0100

    hero_section adjusted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -69,9 +69,9 @@ const Navbar = () => {
                     </nav>
                     
                     <div className="container-fluid aboutt text-center ">
-                            <h1 className="namee">NWANKWO EMMANUEL</h1>
+                            <h1 className="namee mt-5">NWANKWO EMMANUEL</h1>
                         <p> <span className="mee1 ">FULLSTACK &nbsp; PYTHON &nbsp; WEB &nbsp; DEVELOPER </span> <br /></p>
-                        <a href="" className="btn btn-warning btn-center mt-2">Download Resume</a>
+                        <a href="../assets2/Emma_Resume.pdf" className="btn btn-warning btn-center mt-2">Download Resume</a>
                     <p> <span className="typed" data></span></p>
                             <a className=" btn btn-center " href="#projects"><i className="fa fa-arrow-down gradient p-2 arrow"></i></a>
                     </div>
